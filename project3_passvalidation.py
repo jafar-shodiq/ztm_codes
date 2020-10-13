2cb62399df6049e6cc8639f98ba83b1a41e4d1c1
Jafar Shodiq <j.shodiq2312@gmail.com>
2020-10-13 13:33:50 +0700

    removed a comment

--- project3_passvalidation.py
+++ project3_passvalidation.py
@@ -3,7 +3,7 @@ import re
 #at least 8 char long
 #contain any sort letters, numbers, $%#@
 
-pattern = re.compile(r"[A-Za-z0-9!@#$%^&+=]{8,}") #password validation
+pattern = re.compile(r"[A-Za-z0-9!@#$%^&+=]{8,}")
 string = input('enter your password: ')
 
 a = pattern.fullmatch(string)
