7ef3be5f23cb8dd631cda79e26003ef5be697122
Jafar Shodiq <j.shodiq2312@gmail.com>
2020-10-13 15:52:47 +0700

    first commit

---
+++ project8_passwordchecker.py
@@ -0,0 +1,35 @@
+import requests
+import hashlib
+
+def request_api_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error fetching: {res.status_code}, check the API and try again')
+    return res
+
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h,count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
+
+def pwned_api_check(password):
+    # check password if it exists in API response
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first5char, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5char)
+    return get_password_leaks_count(response, tail)
+
+def main(args):
+    count = pwned_api_check(input)
+    if count:
+        print(f'{input} was found {count} times. You should probably change your password.')
+    else:
+        print(f'{input} was NOT found. Carry on!')
+    return 'all done'
+
+if __name__ == '__main__':
+    input = str(input('Check your password: '))
+    main(input)
